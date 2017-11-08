e2a490a550663d4943ca44b811c0b1b473864965
Jon Drew <jon.drew1492@gmail.com>
2017-11-08 15:59:21 +0000

    Added delete button.

--- express_server.js
+++ express_server.js
@@ -2,11 +2,14 @@ let express = require("express");
 let app = express();
 let PORT = process.env.PORT || 8080; // default port 8080
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs", "body-parser")
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 function databasteToArray(object) {
@@ -17,13 +20,13 @@ function databasteToArray(object) {
   } return urlList;
 }
 
-/*function generateRandomString() {
+function generateRandomString() {
   let validChars = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
   let randomString = ''
   for (i = 0; i < 6; i++) {
-    randomString += validChars[Math.floor((Math.random() * 62) + 1)
+    randomString += validChars[Math.floor((Math.random() * 62) + 1)]
   } return randomString
-}*/
+}
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: databasteToArray(urlDatabase),
@@ -35,14 +38,6 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-/*app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id,
-                       fullURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});*/
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/urls/new", (req, res) => {
   let templateVars = { urls: databasteToArray(urlDatabase),
@@ -52,7 +47,14 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  res.send("Ok");
+  urlDatabase[generateRandomString()] = req.body["longURL"];
+  console.log(urlDatabase);
+  res.redirect(req.body["longURL"]);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.redirect(urlDatabase[req.params.shortURL]);
 });
 
 app.listen(PORT, () => {
