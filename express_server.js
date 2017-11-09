61b9b8565cf2168a032441961f46d6c683de055f
Jon Drew <jon.drew1492@gmail.com>
2017-11-09 04:59:55 +0000

    Bug fixes

--- express_server.js
+++ express_server.js
@@ -35,7 +35,7 @@ function generateTinyURL() {
 // Creates main page (http://localhost:8080/urls/) with
 // data from urlDatabase object rendered by urls_index.ejs and _header.ejs partial
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase};
+  let templateVars = { urlDatabase: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
@@ -51,8 +51,8 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// Adds an entry to urlDatabase using POST request via form and
-// redirects user to the new url
+// Adds an entry to urlDatabase using POST request
+// via form in urls_new.ejs and redirects user to the new url
 app.post("/urls", (req, res) => {
   urlDatabase[generateTinyURL()] = req.body["longURL"];
   res.redirect(req.body["longURL"]);
@@ -65,7 +65,8 @@ app.get("/u/:id", (req, res) => {
   res.redirect(urlDatabase[req.params.id]);
 });
 
-// Delete functionality using POST request via form
+// Delete functionality using POST request
+// via form in urls_index.ejs
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls/");
@@ -74,15 +75,17 @@ app.post("/urls/:id/delete", (req, res) => {
 // Creates a page for an individual URL
 // rendered by urls_show.ejs when link is clicked
 app.get('/urls/:id', (req, res) => {
-  let templateVars = {id: req.params.id,
-                      longURL: urlDatabase[id]};
+  let templateVars = {urlDatabase: urlDatabase,
+                      id: req.params.id,
+                      longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
-// Edit functionality using POST request via form
+// Edit functionality using POST request
+// via form in urls_show.ejs
 app.post('/urls/:id', (req, res) => {
-  const longURL = req.body
-  urlDatabase[id] = req.body;
+  console.log(req.body.longURL)
+  urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls/");
 });
 
